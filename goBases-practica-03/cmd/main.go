367f1b5b59b32280eda4745e4f9fc57c421bc328
iYoarihtMacedo <irvin.macedo@mercadolibre.com.mx>
2024-01-02 20:18:37 -0600

    feat: practice 03 from live class correction

--- goBases-practica-03/cmd/main.go
+++ goBases-practica-03/cmd/main.go
@@ -2,4 +2,25 @@ package main
 
 func main() {
 
+	// var 1firstName string -> los nombres de variables no inician con numeros
+	var firstName string
+
+	// Esta variable esta correcta
+	var lastName string
+
+	// Esta variable esta correcta
+	var int age
+
+	// 1lastName := 6 -> las variables no deben iniciar con numeros y deberia ser un string esta variable
+	lastName1 := "Macedo"
+
+	// var driver_license = true -> se debe seguir camel case para la declaracion de variables y falta agregar el tipo de dato
+	var driverLicense bool = true
+
+	// var person height int -> las variables no llevan espacios para separar palabras que las componen y el dato deberia ser un float
+	var personHeight float64
+
+	// Esta variable es correcta
+	childsNumber := 2
+
 }
