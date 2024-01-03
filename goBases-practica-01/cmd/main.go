9c7a5f757c36da296e6f474f65a2988e193e4f45
iYoarihtMacedo <irvin.macedo@mercadolibre.com.mx>
2024-01-02 20:00:47 -0600

    feat: practice 01

---
+++ goBases-practica-01/cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	name := "Irvin Yoariht Macedo Cruz"
+	address := "Higueras Mz 4 Lt 47, Los Heroes Coacalco 1ra Seccion, Coacalco de Berriozabal, 55712"
+
+	fmt.Println("name: ", name)
+	fmt.Println("address: ", address)
+
+}
