dad5b40561e2327691eaa8679a32b0af30191b1e
iYoarihtMacedo <irvin.macedo@mercadolibre.com.mx>
2024-01-02 20:25:40 -0600

    feat: practice 04 from live class

--- goBases-practica-04/cmd/main.go
+++ goBases-practica-04/cmd/main.go
@@ -2,4 +2,20 @@ package main
 
 func main() {
 
+	// La variable es correcta
+	var lastName string = "Smith"
+
+	// var age int = "35" -> Se declara un int por lo que deberia ser el valor sin las comillas
+	var age int = 35
+
+	// boolean := "false" -> boolean es el tipo de dato, hace falta brindar un nombre a la variable
+	flag := false        // Sol1
+	var val bool = false // Sol2
+
+	// var salary string = 45857.90 -> la variable se declara como un string pero por el valor deberia ser un float64
+	var salary float64 = 45857.90
+
+	// La variable es correcta
+	var firstName string = "Mary"
+
 }
