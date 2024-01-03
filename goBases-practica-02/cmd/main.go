119e5ef64e71e670d8685a7661e29b9d1bbe6a06
iYoarihtMacedo <irvin.macedo@mercadolibre.com.mx>
2024-01-02 20:08:17 -0600

    feat: practice 02 from live class

---
+++ goBases-practica-02/cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var temp float64 = 17.0
+	var wet int = 34
+	var pres int = 1020
+
+	fmt.Println("temperature: ", temp)
+	fmt.Println("wet: ", wet)
+	fmt.Println("pressure: ", pres)
+
+}
